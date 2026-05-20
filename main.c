d16831d5622c2f65b12c29754783fb64ec888be0
alampner <andrewlampner@gmail.com>
2026-05-20 09:36:58 -0400

    update prior to working in VM

--- main.c
+++ main.c
@@ -5,4 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {}
+int main() {
+    FILE* logFile = fopen("OpenStack_2k.log", "r");
+    if (logFile == NULL){
+        printf("Log file failed to load");
+        return 1;
+    }
+
+    char* line = NULL;
+    size_t length = 0;
+    ssize_t lineRead;
+
+    lineRead = getline(&line, &length, logFile);
+    printf("%s", line);
+}
